da0df8a500a5d607501a86996179361c4420746f
Alec Patterson <8888alec@gmail.com>
2023-10-26 13:27:52 -0400

    Front End CORS

--- Client/quasar.config.js
+++ Client/quasar.config.js
@@ -87,8 +87,18 @@ module.exports = configure(function (/* ctx */) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#devServer
     devServer: {
+      proxy: {
+        // proxy all requests starting with /api to Django backend
+        '/api': {
+          target: 'http://localhost:8000',
+          changeOrigin: true,
+          pathRewrite: {
+            '^/api': ''
+          }
+        }
+      },
       // https: true
-      open: true // opens browser window automatically
+      open: true, // opens browser window automatically
     },
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#framework
