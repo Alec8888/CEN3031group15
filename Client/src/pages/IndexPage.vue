50ed2fbd5042756daba294065e1feff7bd515946
Alec Patterson <8888alec@gmail.com>
2023-10-26 11:53:25 -0400

    styling home page

--- Client/src/pages/IndexPage.vue
+++ Client/src/pages/IndexPage.vue
@@ -1,14 +1,27 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page>
     <img
       alt="PantryPal logo"
       src="~assets/PantryPal-Logo.png"
       style="width: 700px; height: 700px"
     >
 
+    <div class = "q-pa-md text-h3 text-primary" >
+      PantryPal
+    </div>
+
+    <div class = "q-pa-md text-body1" >
+      A food pantry management system.
+    </div>
+
+    <div class="q-pa-md qgutter-sm">
+      <q-btn color="primary" glossy label="Sign In" to="signUp" />
+    </div>
+
     <div class="q-pa-md qgutter-sm">
       <q-btn color="secondary" glossy label="Sign up" to="signUp" />
     </div>
+    
   </q-page>
 </template>
 
