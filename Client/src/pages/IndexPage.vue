9bf35428e73417a9b801ec85f528b03224d26b0a
Aly <alyssaperkins@ufl.edu>
2023-11-07 19:31:52 -0500

    Updated home page and sign in page

--- Client/src/pages/IndexPage.vue
+++ Client/src/pages/IndexPage.vue
@@ -1,18 +1,41 @@
 <template>
-  <q-page padding>
-
-    <div class = "text-body1 q-mt-md" >
-      A food donation platform.
+  <q-page class="q-gutter-md" style="height: 100vh; display: flex; flex-direction: column; justify-content: center; align-items: center;">
+    <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; margin-bottom: 20px;">
+      <img
+        alt="PantryPal logo"
+        src="~assets/Cropped-Pantry-Pal-Logo.png"
+        style="max-width: 60%; height: auto; margin-bottom: 10px; margin-top: 100px"
+      >
     </div>
-
-    <img
-      alt="PantryPal logo"
-      src="~assets/PantryPal-Logo.png"
-      style="width: 700px; height: 700px"
-    >
-    
+    <div class="text-h7 q-mb-md">
+      <div class="welcome-text custom-hammersmith">
+        Welcome to Pantry Pal, your go-to food donation web app designed with a mission to make a difference! 
+        Here at Pantry Pal, we're passionate about connecting local community organizations with donation centers, 
+        empowering them to collaborate seamlessly in the fight against food waste. Created by a team of dedicated 
+        students from the University of Florida, our app is a testament to our commitment to reducing food waste 
+        and alleviating hunger in the United States. With Pantry Pal, you join a community-driven initiative aimed 
+        at fostering meaningful connections, promoting sustainability, and making a positive impact on the lives 
+        of those in need. Together, let's create a world where no one goes hungry, and where surplus food finds 
+        its way to those who need it the most. Thank you for joining us!
+      </div>
+    </div>
   </q-page>
 </template>
+
+<style scoped>
+
+.welcome-text {
+  text-align: center;
+  padding: 0 10vw;
+  margin-bottom: 50px;
+}
+
+.custom-hammersmith {
+  font-family: 'Hammersmith One', sans-serif;
+  font-size: 2em;
+  font-weight: normal;
+}
+</style>
 
 <script>
 import { defineComponent } from 'vue'
--- Client/src/pages/sign_in.vue
+++ Client/src/pages/sign_in.vue
@@ -2,55 +2,59 @@
   <q-page class="column items-center">
   <!-- <div class="column items-center"> -->
 
-    <q-card>
-      <!-- <q-card-section>
-        <img
-          alt="PantryPal logo"
-          src="~assets/PantryPal-Logo.png"
-          style="width: 300px; height:300px"
-        >
-      </q-card-section> -->
-      <q-card-section>
-        <div class="text-subtitle2">Sign in to PantryPal</div>
-      </q-card-section>
+    <div class="self-center">
+      <q-card>
+        <!-- <q-card-section>
+          <img
+            alt="PantryPal logo"
+            src="~assets/PantryPal-Logo.png"
+            style="width: 300px; height:300px"
+          >
+        </q-card-section> -->
+        <q-card-section>
+          <div class="custom-hammersmith">Sign In to PantryPal</div>
+        </q-card-section>
 
-      <q-card-section>
-        <q-form
-          action="http://localhost:8000/#/api"
-          method="post"
-          @submit="onSubmit"
-          @reset="onReset"
-          class="q-gutter-sm"
-        >
-          <q-input
-            filled
-            type="email"
-            v-model="email"
-            label="Email address*"
-            lazy-rules
-            color="secondary"
-            :rules="[
-              val => val && val.length > 0 || 'Please type something',
-              val => /^.+@.+\..+$/.test(val) || 'Please type a valid email'
-            ]"
-          />
-    
-          <q-input
+        <q-card-section>
+          <q-form
+            action="http://localhost:8000/#/api"
+            method="post"
+            @submit="onSubmit"
+            @reset="onReset"
+            class="q-gutter-sm"
+          >
+            <q-input
               filled
-              v-model="password"
-              label="Password*"
+              type="email"
+              v-model="email"
+              label="Email address*"
               lazy-rules
               color="secondary"
-              :rules="[ val => val && val.length > 0 || 'Please type something',
-                        val => val && val.length > 7 || 'Password must be at least 8 characters long']"
+              :rules="[
+                val => val && val.length > 0 || 'Please type something',
+                val => /^.+@.+\..+$/.test(val) || 'Please type a valid email'
+              ]"
             />
-    
-            <q-btn label="Submit" type="submit" color="primary"/>
-        </q-form>
+      
+            <q-input
+                filled
+                v-model="password"
+                label="Password*"
+                lazy-rules
+                color="secondary"
+                :rules="[ val => val && val.length > 0 || 'Please type something',
+                          val => val && val.length > 7 || 'Password must be at least 8 characters long']"
+              />
+      
+              <div class="button-container">
+                <q-btn label="Submit" type="submit" color="primary" />
+              </div>
+          </q-form>
 
-      </q-card-section>
+        </q-card-section>
 
-    </q-card>
+      </q-card>
+    </div>
     
     
 
@@ -58,6 +62,31 @@
   <!-- </div> -->
   </q-page>
 </template>
+
+<style scoped>
+.column.items-center {
+  height: 200px;
+}
+
+.self-center {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+
+.custom-hammersmith {
+  font-family: 'Hammersmith One', sans-serif;
+  font-size: 2em;
+}
+
+.button-container {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+}
+</style>
+
 
 <script>
 import { useQuasar } from 'quasar'
@@ -100,7 +129,7 @@ export default {
                           color: 'green-4',
                           textColor: 'white',
                           icon: 'cloud_done',
-                          message: 'Log in successfull'
+                          message: 'Log In Successful'
                       });
                   }
             },
