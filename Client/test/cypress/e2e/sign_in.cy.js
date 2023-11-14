19384b339a16461951df56093166c0a123d3e853
Alec Patterson <8888alec@gmail.com>
2023-11-14 10:38:37 -0500

    fixed sign in cypress test

--- Client/test/cypress/e2e/sign_in.cy.js
+++ Client/test/cypress/e2e/sign_in.cy.js
@@ -3,47 +3,24 @@ describe('Sign In Page', () => {
       cy.visit('/signin')
     })
 
-
     it('Loads the sign in page', () => {
         cy.testRoute('/signin')
     })
   
-    it('Has an email input', () => {
+    it('Has all needed elements', () => {
         cy.dataCy('email-input').should('exist');
-    });
+        cy.dataCy('password-input').should('exist');
+        cy.get('button[type="submit"]').should('exist')
+        cy.dataCy('link_register').should('exist')
+    })
 
-
-    it('Can type into email input', () => {
+    it('Can sign in', () => {
         cy.dataCy('email-input').type('a@gmail.com');
-    });
-
-    it('Has a password input', () => {
-      cy.dataCy('password-input').should('exist')
-    })
-
-    it('Can type in password input', () => {
         cy.dataCy('password-input').type('qwerasdf');
-    });
-    
-    it('Has a submit button', () => {
-      cy.get('button[type="submit"]').should('exist')
-    })
-  
-    it('Can click the submit button', () => {
-      cy.get('button[type="submit"]').click()
+        cy.get('button[type="submit"]').click()
+        cy.testRoute('/home')
     })
 
-    it('Create account link routes to register page', () => {
-        cy.dataCy('link_register').click()
-        cy.testRoute('/register')
-  })
 
-  it('Can sign in', () => {
-    cy.dataCy('email-input').type('a@gmail.com');
-    cy.dataCy('password-input').type('qwerasdf');
-    cy.get('button[type="submit"]').click()
-    cy.testRoute('/home')
-
-  })
 
 })
