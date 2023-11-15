e822416467a797cb7efac69c9c4380a5c2957890
Alec Patterson <8888alec@gmail.com>
2023-11-15 17:26:21 -0500

    fix for json back end log in

--- Client/src/pages/sign_in.vue
+++ Client/src/pages/sign_in.vue
@@ -87,7 +87,7 @@ export default defineComponent({
         const onSubmit = async () => {
           console.log('log in clicked: ' + email.value + ' ' + password.value)
 
-          let response = await fetch('http://localhost:8000/home/', {
+          let response = await fetch('http://localhost:3000/signin', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
